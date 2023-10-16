02cef76dc98dfb31c9ed536d923e43d7b2d6f582
Dh-Pardeep <pardeepjangra514@gmail.com>
2023-10-16 14:46:35 +0530

    sachin section add

--- src/components/view/HomePage.jsx
+++ src/components/view/HomePage.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import HeaderNav from '../common/HeaderNav'
 import HeroSection from '../AllSection/HeroSection'
+import AboutYou from '../AllSection/AboutYou'
+import AboutUs from '../AllSection/AboutUs'
+import React from "react";
+import WhyUs from "../AllSection/WhyUs";
+import MyFooter from "../common/MyFooter";
+import HeaderNav from "../common/HeaderNav";
+import HeroSection from "../AllSection/HeroSection";
 
 const HomePage = () => {
   return (
     <>
-    <HeroSection/>
+      <HeroSection />
+      <AboutYou />
+      <AboutUs />
+      <WhyUs />
+      <MyFooter />
     </>
+
+
   )
 }
 
