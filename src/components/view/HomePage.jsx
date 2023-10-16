f85956de9c06a1d1a850b76ed006fe4461107789
Dh-Pardeep <pardeepjangra514@gmail.com>
2023-10-16 14:46:55 +0530

    three marge

--- src/components/view/HomePage.jsx
+++ src/components/view/HomePage.jsx
@@ -1,12 +1,8 @@
 import React from 'react'
-import HeaderNav from '../common/HeaderNav'
-import HeroSection from '../AllSection/HeroSection'
 import AboutYou from '../AllSection/AboutYou'
-import AboutUs from '../AllSection/AboutUs'
-import React from "react";
+import AboutUs from '../AllSection/AboutUs' 
 import WhyUs from "../AllSection/WhyUs";
 import MyFooter from "../common/MyFooter";
-import HeaderNav from "../common/HeaderNav";
 import HeroSection from "../AllSection/HeroSection";
 
 const HomePage = () => {
