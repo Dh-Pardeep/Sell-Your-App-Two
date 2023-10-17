4f34ab12c742018d3056f99bd09b1267668e47ca
Dh-Pardeep <pardeepjangra514@gmail.com>
2023-10-17 15:47:33 +0530

    nav bar font  add

--- src/components/homepage/Partnership.jsx
+++ src/components/homepage/Partnership.jsx
@@ -7,7 +7,7 @@ const Partnership = () => {
     return (
         <div className='bg-Hero-bg sm:py-10 py-6 lg:pt-[100px] lg:pb-[120px]'>
             <div className='container mx-auto'>
-                <h2 className='mb-[60px] text-center leading-[170%] font-Manrope text-[24px] md:text-[28px] lg:text-[30px] xl:text-lg font-medium text-white  ' data-aos="zoom-in" data-aos-duration="1000" data-aos-delay="500" data-aos-easing="linear">Partnership</h2>
+                <h2 className='lg:mb-[60px] text-center leading-[170%] font-Manrope text-[24px] md:text-[28px] lg:text-[30px] xl:text-lg font-medium text-white  ' data-aos="zoom-in" data-aos-duration="1000" data-aos-delay="500" data-aos-easing="linear">Partnership</h2>
                 <div className='w-full flex flex-col lg:flex-row items-center lg:mb-[134px] mb-10'>
                     <div className='w-full lg:w-6/12 lg:mb-0 mb-10'  data-aos="fade-right" data-aos-duration="1000" data-aos-delay="500" data-aos-easing="linear">
                         <h2 className='font-Manrope text-[24px] md:text-[28px] lg:text-[30px] xl:text-lg font-medium text-white mb-4'>Over $5 billion of transactions protected with escrow.com</h2>
@@ -19,31 +19,31 @@ const Partnership = () => {
                     </div>
                 </div>
                 <div className='flex xl:gap-10 gap-5 justify-center xl:flex-nowrap flex-wrap'>
-                    <div className='partnership_box relative z-[2] rounded-[8px] border border-[rgba(62_51_73)] hover:border-[rgb(51_23_63)] transition duration-400 ease-linear cursor-pointer px-5 py-5 w-[207px]' data-aos="zoom-in" data-aos-duration="1000" data-aos-delay="500" data-aos-easing="linear">
+                    <div className='partnership_box relative z-[2] rounded-[8px] border border-[rgba(62_51_73)] hover:border-[rgb(51_23_63)] transition duration-400 ease-linear cursor-pointer px-5 py-5 w-[207px]' >
                         <span className='partnership_ellipse hidden w-[132px] h-[134px] absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] z-[-1]'></span>
                         <span className='absolute top-[-37%] right-[-22%] xl:block hidden'><LineUP/></span>
                         <span className='w-[64px] h-[64px] rounded-[50px] bg-white flex justify-center items-center'><Terms /></span>
                         <p className='leading-[170%] font-Inter text-[15px] lg:text-sm font-normal text-white mt-4'>Buyer and Seller agree to terms</p>
                     </div>
-                    <div className='partnership_box z-[2] relative rounded-[8px] border border-[rgba(62_51_73)] hover:border-[rgb(51_23_63)] transition duration-400 ease-linear cursor-pointer px-5 py-5 w-[207px] xl:translate-y-[-80px]' data-aos="zoom-in" data-aos-duration="1000" data-aos-delay="500" data-aos-easing="linear">
+                    <div className='partnership_box z-[2] relative rounded-[8px] border border-[rgba(62_51_73)] hover:border-[rgb(51_23_63)] transition duration-400 ease-linear cursor-pointer px-5 py-5 w-[207px] xl:translate-y-[-80px]' >
                         <span className='partnership_ellipse hidden w-[132px] h-[134px] absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] z-[-1]'></span>
                     <span className='absolute bottom-[-37%] right-[-22%] xl:block hidden'><LineDown/></span>
                         <span className='w-[64px] h-[64px] rounded-[50px] bg-white flex justify-center items-center'><Payments /></span>
                         <p className='leading-[170%] font-Inter text-[15px] lg:text-sm font-normal text-white mt-4'>Buyer submits payment to Escrow</p>
                     </div>
-                    <div className='partnership_box z-[2] relative rounded-[8px] border border-[rgba(62_51_73)] hover:border-[rgb(51_23_63)] transition duration-400 ease-linear cursor-pointer px-5 py-5 w-[207px]' data-aos="zoom-in" data-aos-duration="1000" data-aos-delay="500" data-aos-easing="linear">
+                    <div className='partnership_box z-[2] relative rounded-[8px] border border-[rgba(62_51_73)] hover:border-[rgb(51_23_63)] transition duration-400 ease-linear cursor-pointer px-5 py-5 w-[207px]' >
                         <span className='partnership_ellipse hidden w-[132px] h-[134px] absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] z-[-1]'></span>
                     <span className='absolute top-[-37%] right-[-22%] xl:block hidden'><LineUP/></span>  
                         <span className='w-[64px] h-[64px] rounded-[50px] bg-white flex justify-center items-center'><Service /></span>
                         <p className='leading-[170%] font-Inter text-[15px] lg:text-sm font-normal text-white mt-4'>Seller delivers goods or service to buyer</p>
                     </div>
-                    <div className='partnership_box z-[2] relative rounded-[8px] border border-[rgba(62_51_73)] hover:border-[rgb(51_23_63)] transition duration-400 ease-linear cursor-pointer px-5 py-5 w-[207px] xl:translate-y-[-80px]' data-aos="zoom-in" data-aos-duration="1000" data-aos-delay="500" data-aos-easing="linear">
+                    <div className='partnership_box z-[2] relative rounded-[8px] border border-[rgba(62_51_73)] hover:border-[rgb(51_23_63)] transition duration-400 ease-linear cursor-pointer px-5 py-5 w-[207px] xl:translate-y-[-80px]' >
                         <span className='partnership_ellipse hidden w-[132px] h-[134px] absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] z-[-1]'></span>
                     <span className='absolute bottom-[-37%] right-[-22%] xl:block hidden'><LineDown/></span>
                         <span className='w-[64px] h-[64px] rounded-[50px] bg-white flex justify-center items-center'><Approve /></span>
                         <p className='leading-[170%] font-Inter text-[15px] lg:text-sm font-normal text-white mt-4'>Buyer approves goods or services</p>
                     </div>
-                    <div className='partnership_box z-[2] rounded-[8px] border border-[rgba(62_51_73)] hover:border-[rgb(51_23_63)] transition duration-400 ease-linear cursor-pointer px-5 py-5 w-[207px]' data-aos="zoom-in" data-aos-duration="1000" data-aos-delay="500" data-aos-easing="linear">
+                    <div className='partnership_box z-[2] rounded-[8px] border border-[rgba(62_51_73)] hover:border-[rgb(51_23_63)] transition duration-400 ease-linear cursor-pointer px-5 py-5 w-[207px]' >
                         <span className='partnership_ellipse hidden w-[132px] h-[134px] absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] z-[-1]'></span>
                         <span className='w-[64px] h-[64px] rounded-[50px] bg-white flex justify-center items-center'><Seller /></span>
                         <p className='leading-[170%] font-Inter text-[15px] lg:text-sm font-normal text-white mt-4'>Escrow.com releases payment to seller</p>
