2ef2858096bc165b2acfd9f33113f6f8f5e2b035
sachin-niyana <sachinjakhar100@gmail.com>
2023-10-17 13:56:20 +0530

    add shadow

--- src/components/view/HomePage.jsx
+++ src/components/view/HomePage.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import WhyUs from "../AllSection/WhyUs";
-import MyFooter from "../common/MyFooter";
 import HeaderNav from "../common/HeaderNav";
 import HeroSection from "../AllSection/HeroSection";
 import AppToday from "../AllSection/AppToday";
 import Explore from "../AllSection/Explore";
+import Footer from "../common/Footer";
 
 const HomePage = () => {
   return (
     <>
-      {/* <HeroSection /> */}
+      <HeroSection />
       <WhyUs />
       <Explore />
       <AppToday />
-      <MyFooter />
+      <Footer />
     </>
   );
 };
