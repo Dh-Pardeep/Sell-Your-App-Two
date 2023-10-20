b23958fa66637c544c94caf287362f0529d93754
anilkumar70820 <ak9565191@gmail.com>
2023-10-20 11:18:43 +0530

    mistake correct

--- src/components/homepage/AboutYou.jsx
+++ src/components/homepage/AboutYou.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 
 const AboutYou = () => {
     return (
-        <div className='py-10 lg:py-[104px]'>
-            <div className='container mx-auto flex justify-center'>
-                <div className='relative about_bg sm:p-[40px] p-[12px] w-[922px] rounded-[40px]'>
+        <div className='py-14 lg:py-[104px]'>
+            <div className='container mx-auto flex justify-center xl:w-[1140px] 2xl:w-[1320px] px-3 sm:px-5 xl:px-0'>
+                <div className='relative about_bg sm:p-10 px-3 py-7 w-[922px] rounded-[40px]'>
                     <div className='absolute top-[36px] left-[20px] sm:w-[193px] w-[100px] sm:h-[195px] h-[100px] rounded-[137px] opacity-50 about_ellipse'></div>
                     <div className='absolute bottom-[-15px] right-[20px] sm:w-[193px] sm:h-[195px] rounded-[137px] opacity-50 about_ellipse'></div>
                     <h2 className="text-dark text-center text-[24px] md:text-[28px] lg:text-[30px] xl:text-lg font-medium mb-4 font-Manrope">Tell us about your app</h2>
                     <p className='leading-[170%] text-center text-[15px] lg:text-sm font-normal text-dark opacity-70 sm:mb-[44px] mb-4 font-Inter max-w-[890px]'> If you have a non-gaming ios app with 5000+ monthly downloads and it's been on the App Store for more than a year, we're interested in buying it!</p>
                     <div className='sm:px-[23px] sm:mb-[44px] mb-4'>
                         <div className='flex flex-col sm:flex-row sm:gap-6 gap-3 sm:mb-6 mb-3'>
-                            <input className='outline-none leading-[170%] px-2 w-full text-[15px] lg:text-sm font-normal opacity-90 text-black-light font-Inter bg-white leading-[174%] ps-[10px] pt-[10px] pb-[7px] rounded-lg border border-[rgba(28_28_28_0.08)]' type="text" placeholder='Your Name' />
-                            <input required className='outline-none leading-[170%] px-2 w-full text-[15px] lg:text-sm font-normal opacity-90 text-black-light font-Inter bg-white leading-[174%] ps-[10px] pt-[10px] pb-[7px] rounded-lg border border-[rgba(28_28_28_0.08)]' type="text" placeholder='Your Email' />
+                            <input className='outline-none leading-[170%] px-2 w-full text-[15px] lg:text-sm font-normal opacity-90 text-black-light font-Inter bg-white ps-[10px] pt-[10px] pb-[7px] rounded-lg border border-[rgba(28_28_28_0.08)]' type="text" placeholder='Your Name' />
+                            <input required className='outline-none leading-[170%] px-2 w-full text-[15px] lg:text-sm font-normal opacity-90 text-black-light font-Inter bg-white ps-[10px] pt-[10px] pb-[7px] rounded-lg border border-[rgba(28_28_28_0.08)]' type="text" placeholder='Your Email' />
                         </div>
                         <div className='flex sm:gap-6 gap-3 sm:mb-6 mb-3 flex-col sm:flex-row'>
                             <input required className='outline-none leading-[170%] px-2 w-full text-[15px] lg:text-sm font-normal opacity-90 text-black-light font-Inter bg-white ps-[10px] pt-[10px] pb-[7px] rounded-lg border border-[rgba(28_28_28_0.08)]' type="text" placeholder='Revenue Last Month' />
@@ -21,8 +21,8 @@ const AboutYou = () => {
                         <input required className='outline-none leading-[170%] px-2 w-full sm:mb-6 mb-3 text-[15px] lg:text-sm font-normal opacity-90 text-black-light font-Inter bg-white ps-[10px] pt-[10px] pb-[7px] rounded-lg border border-[rgba(28_28_28_0.08)]' type="text" placeholder='App URL...' />
                         <textarea className='resize-none outline-none leading-[170%] px-2 w-full height-[74px] text-[15px] lg:text-sm font-normal opacity-90 text-black-light font-Inter bg-white ps-[10px] pt-[10px] pb-[7px] rounded-lg border border-[rgba(28_28_28_0.08)]' type="text" placeholder='Anything else you’d like us to know...' />
                     </div>
-                    <div className='flex justify-center sm:mb-[44px] mb-4'><button className='text-white text-[14px] md:text-base font-bold font-Manrope leading-[170%] inline-block px-10 py-4 bg-gradient-to-r from-red-500 to-indigo-600 hover:to-red-500 hover:from-indigo-600 rounded-[38px] transition-all ease-linear duration-300 '>Submit Now</button></div>
-                    <div className='flex justify-center sm:pb-0 pb-3'><p className='font-Manrope text-center about_text opacity-70 border border-[#FF3939] capitalize text-[17px] md:text-[18px] xl:text-xmd sm:px-[40px] px-1 sm:py-5 py-2 rounded-xl inline-block'>We guarantee to give you an offer within 48 hours</p></div>
+                    <div className='flex justify-center sm:mb-[44px] mb-4'><button aria-label='submit' className='text-white text-xsm md:text-sm font-bold font-Manrope leading-[170%] inline-block px-10 py-4 bg-gradient-to-r from-red-500 to-indigo-600 hover:to-red-500 hover:from-indigo-600 rounded-[38px] transition-all ease-linear duration-300 '>Submit Now</button></div>
+                    <div className='flex justify-center sm:pb-0 pb-3'><p className='font-Manrope text-center about_text opacity-70 border border-[#FF3939] capitalize text-[17px] md:text-[18px] xl:text-xmd sm:px-10 px-3 sm:py-5 py-3 rounded-xl inline-block'>We guarantee to give you an offer within 48 hours</p></div>
                 </div>
             </div>
         </div>
--- src/components/homepage/Partnership.jsx
+++ src/components/homepage/Partnership.jsx
@@ -1,49 +1,59 @@
 import React from 'react'
 import phone_img from '../../assets/img/png/phone_img.png'
 import { Approve, LineDown, LineUP, Payments, Seller, Service, Terms } from '../common/Icon'
-// import bg_ellipse from '../../assets/img/svg/bg_elipse.svg'
-
+// import { Tilt } from 'react-tilt'
 const Partnership = () => {
+    // const defaultOptions = {
+    //     reverse: false,  // reverse the tilt direction
+    //     max: 35,     // max tilt rotation (degrees)
+    //     perspective: 1000,   // Transform perspective, the lower the more extreme the tilt gets.
+    //     scale: .01,    // 2 = 200%, 1.5 = 150%, etc..
+    //     speed: 1000,   // Speed of the enter/exit transition
+    //     transition: true,   // Set a transition on enter/exit.
+    //     axis: null,   // What axis should be disabled. Can be X or Y.
+    //     reset: true,    // If the tilt effect has to be reset on exit.
+    //     easing: "cubic-bezier(.03,.98,.52,.99)",    // Easing on enter/exit.
+    // }
     return (
-        <div className='bg-Hero-bg sm:py-10 py-6 lg:pt-[100px] lg:pb-[120px]'>
-            <div className='container mx-auto'>
-                <h2 className='mb-[60px] text-center leading-[170%] font-Manrope text-[24px] md:text-[28px] lg:text-[30px] xl:text-lg font-medium text-white mb-4'>Partnership</h2>
+        <div className='bg-Hero-bg py-14 lg:pt-[100px] lg:pb-[120px]'>
+            <div className='container mx-auto xl:w-[1140px] 2xl:w-[1320px] px-3 sm:px-5 xl:px-0'>
+                <h2 className='lg:mb-[60px] text-center leading-[170%] font-Manrope text-[24px] md:text-[28px] lg:text-[30px] xl:text-lg font-medium text-white mb-4'>Partnership</h2>
                 <div className='w-full flex flex-col lg:flex-row items-center lg:mb-[134px] mb-10'>
-                    <div className='w-full lg:w-6/12 lg:mb-0 mb-10'>
-                        <h2 className='font-Manrope text-[24px] md:text-[28px] lg:text-[30px] xl:text-lg font-medium text-white mb-4'>Over $5 billion of transactions protected with escrow.com</h2>
-                        <p className='leading-[170%] font-Inter text-[15px] lg:text-sm font-normal text-white opacity-70 mb-4'>Escrow.com is the world’s most secure payment method <span className='lg:block'> from a counterparty risk perspective - safeguarding both</span> buyer and seller, all funds transacted using
-                            <span className='lg:block'> escrow are kept in trust.</span></p>
+                    <div className='w-full lg:w-6/12'>
+                        <h2 className='font-Manrope text-[24px] md:text-[28px] lg:text-[30px] xl:text-lg font-medium text-white mb-4 lg:max-w-[480px] text-center lg:text-start'>Over $5 billion of transactions protected with <a className='hover:text-light-black' href="https://escrow.com"> escrow.com </a></h2>
+                        <p className='lg:max-w-[437px] leading-[170%] font-Inter text-[15px] lg:text-sm font-normal text-white opacity-70 mb-4 lg:text-start text-center'>Escrow.com is the world’s most secure payment method from a counterparty risk perspective - safeguarding both buyer and seller, all funds transacted using
+                            escrow are kept in trust.</p>
                     </div>
-                    <div className='lg:mt-0 mt-10'>
-                        <img className='hover:scale-[1.01] transition duration-400 ease-linear cursor-pointer' src={phone_img} alt="phone_img" />
-                    </div>
+                        <div className='lg:mt-0 mt-6'>
+                            <img className='hover:scal-[.1]transition duration-400 ease-linear cursor-pointer' src={phone_img} alt="phone_img" />
+                        </div>
                 </div>
                 <div className='flex xl:gap-10 gap-5 justify-center xl:flex-nowrap flex-wrap'>
-                    <div className='partnership_box relative z-[2] rounded-[8px] border border-[rgba(62_51_73)] hover:border-[rgb(51_23_63)] transition duration-400 ease-linear cursor-pointer px-5 py-5 w-[207px]'>
+                    <div className='partnership_box relative z-[2] rounded-[8px] border border-[rgba(62_51_73)] hover:border-[rgb(51_23_63)] transition duration-400 ease-linear cursor-pointer px-4 py-5 w-[207px]'>
                         <span className='partnership_ellipse hidden w-[132px] h-[134px] absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] z-[-1]'></span>
-                        <span className='absolute top-[-37%] right-[-22%] xl:block hidden'><LineUP/></span>
+                        <span className='absolute top-[-37%] right-[-22%] xl:block hidden'><LineUP /></span>
                         <span className='w-[64px] h-[64px] rounded-[50px] bg-white flex justify-center items-center'><Terms /></span>
                         <p className='leading-[170%] font-Inter text-[15px] lg:text-sm font-normal text-white mt-4'>Buyer and Seller agree to terms</p>
                     </div>
-                    <div className='partnership_box z-[2] relative rounded-[8px] border border-[rgba(62_51_73)] hover:border-[rgb(51_23_63)] transition duration-400 ease-linear cursor-pointer px-5 py-5 w-[207px] xl:translate-y-[-80px]'>
+                    <div className='partnership_box z-[2] relative rounded-[8px] border border-[rgba(62_51_73)] hover:border-[rgb(51_23_63)] transition duration-400 ease-linear cursor-pointer ps-4 pe-2 py-5 w-[178px] xl:translate-y-[-80px]'>
                         <span className='partnership_ellipse hidden w-[132px] h-[134px] absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] z-[-1]'></span>
-                    <span className='absolute bottom-[-37%] right-[-22%] xl:block hidden'><LineDown/></span>
+                        <span className='absolute bottom-[-37%] right-[-22%] xl:block hidden'><LineDown /></span>
                         <span className='w-[64px] h-[64px] rounded-[50px] bg-white flex justify-center items-center'><Payments /></span>
                         <p className='leading-[170%] font-Inter text-[15px] lg:text-sm font-normal text-white mt-4'>Buyer submits payment to Escrow</p>
                     </div>
-                    <div className='partnership_box z-[2] relative rounded-[8px] border border-[rgba(62_51_73)] hover:border-[rgb(51_23_63)] transition duration-400 ease-linear cursor-pointer px-5 py-5 w-[207px]'>
+                    <div className='partnership_box z-[2] relative rounded-[8px] border border-[rgba(62_51_73)] hover:border-[rgb(51_23_63)] transition duration-400 ease-linear cursor-pointer ps-4 pe-3 py-5 w-[206px]'>
                         <span className='partnership_ellipse hidden w-[132px] h-[134px] absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] z-[-1]'></span>
-                    <span className='absolute top-[-37%] right-[-22%] xl:block hidden'><LineUP/></span>  
+                        <span className='absolute top-[-37%] right-[-22%] xl:block hidden'><LineUP /></span>
                         <span className='w-[64px] h-[64px] rounded-[50px] bg-white flex justify-center items-center'><Service /></span>
                         <p className='leading-[170%] font-Inter text-[15px] lg:text-sm font-normal text-white mt-4'>Seller delivers goods or service to buyer</p>
                     </div>
-                    <div className='partnership_box z-[2] relative rounded-[8px] border border-[rgba(62_51_73)] hover:border-[rgb(51_23_63)] transition duration-400 ease-linear cursor-pointer px-5 py-5 w-[207px] xl:translate-y-[-80px]'>
+                    <div className='partnership_box z-[2] relative rounded-[8px] border border-[rgba(62_51_73)] hover:border-[rgb(51_23_63)] transition duration-400 ease-linear cursor-pointer ps-4 pe-[10px] py-5 w-[197px] xl:translate-y-[-80px]'>
                         <span className='partnership_ellipse hidden w-[132px] h-[134px] absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] z-[-1]'></span>
-                    <span className='absolute bottom-[-37%] right-[-22%] xl:block hidden'><LineDown/></span>
+                        <span className='absolute bottom-[-37%] right-[-22%] xl:block hidden'><LineDown /></span>
                         <span className='w-[64px] h-[64px] rounded-[50px] bg-white flex justify-center items-center'><Approve /></span>
                         <p className='leading-[170%] font-Inter text-[15px] lg:text-sm font-normal text-white mt-4'>Buyer approves goods or services</p>
                     </div>
-                    <div className='partnership_box z-[2] rounded-[8px] border border-[rgba(62_51_73)] hover:border-[rgb(51_23_63)] transition duration-400 ease-linear cursor-pointer px-5 py-5 w-[207px]'>
+                    <div className='partnership_box z-[2] rounded-[8px] border border-[rgba(62_51_73)] hover:border-[rgb(51_23_63)] transition duration-400 ease-linear cursor-pointer ps-4 pe-3 py-5 w-[190px]'>
                         <span className='partnership_ellipse hidden w-[132px] h-[134px] absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] z-[-1]'></span>
                         <span className='w-[64px] h-[64px] rounded-[50px] bg-white flex justify-center items-center'><Seller /></span>
                         <p className='leading-[170%] font-Inter text-[15px] lg:text-sm font-normal text-white mt-4'>Escrow.com releases payment to seller</p>
